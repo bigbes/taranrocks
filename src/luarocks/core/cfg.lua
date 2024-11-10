1caf5397f6ddbab4bbed8aa58431f0c81d2df0a1
Oleg Jukovec <oleg.jukovec@tarantool.org>
2024-11-10 18:16:57 +0300

    luarocks: fix usage of local configs
    Skipping of load local configuration files will result in problems and
    loss of backward compatibility.
    
    Part of https://github.com/tarantool/tt/issues/1003

--- src/luarocks/core/cfg.lua
+++ src/luarocks/core/cfg.lua
@@ -727,14 +727,10 @@ function cfg.init(detected, warning)
       exit_ok, exit_err, exit_what = nil, err, "config"
    end
 
-   if hardcoded.FORCE_HARDCODED then
-      util.deep_merge(cfg.variables, hardcoded)
-   end
-
    -- Load user configuration file (if allowed)
    local home_config_ok
    local project_config_ok
-   if not hardcoded.FORCE_CONFIG and not hardcoded.FORCE_HARDCODED then
+   if not hardcoded.FORCE_CONFIG then
       local env_var   = "LUAROCKS_CONFIG_" .. cfg.lua_version:gsub("%.", "_")
       local env_value = os.getenv(env_var)
       if not env_value then
@@ -784,6 +780,10 @@ function cfg.init(detected, warning)
             exit_ok, exit_err, exit_what = nil, err, "config"
          end
       end
+   end
+
+   if hardcoded.FORCE_HARDCODED then
+      util.deep_merge(cfg.variables, hardcoded)
    end
 
    -- backwards compatibility:
